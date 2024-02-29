007845e366cafe57d44b0f1d0af6971e050a717e
sjmagbanua <101148338+sjmagbanua@users.noreply.github.com>
2024-03-01 02:56:32 +0800

    wrap ui page blocprovider

--- lib/app/pos/views/_pos_page.dart
+++ lib/app/pos/views/_pos_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gb_pos_store/app/pos/bloc/pos_bloc.dart';
+import 'package:gb_pos_store/app/pos/bloc/pos_state/pos_state.dart';
 import 'package:gb_pos_store/app/pos/views/_pos_views.dart';
+import 'package:gb_pos_store/repositories/item_repository.dart';
 
 class PosPage extends StatefulWidget {
-  const PosPage({super.key});
+  final PosState? initialState;
+  const PosPage({super.key, this.initialState});
 
   @override
   State<PosPage> createState() => _PosPageState();
@@ -15,98 +20,103 @@ class _PosPageState extends State<PosPage> {
 
     // Calculate the height of the container as 75% of the viewport height
     double containerHeight = viewportHeight * 0.75;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('POS System'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Flexible(
-                  flex: 7,
-                  child: Container(
-                    height: containerHeight,
-                    width: double.infinity,
-                    child: const SingleChildScrollView(
-                      child: POSViewTable(),
-                    ),
-                  ),
-                ),
-                Flexible(
-                  flex: 3,
-                  child: Container(
-                    height: containerHeight,
-                    width: double.infinity,
-                    child: Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Column(
-                            children: [
-                              POSViewSearch(),
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
-                                child: POSViewCalculation(),
-                              ),
-                            ],
-                          ),
-                          ElevatedButton(
-                            onPressed: () {
-                              // Add your button onPressed logic here
-                            },
-                            // style: ElevatedButton.styleFrom(
-                            //   primary: Colors.red,
-                            //   minimumSize: const Size(double.infinity,
-                            //       50), // Full width and 50 height
-                            //   shape: RoundedRectangleBorder(
-                            //     borderRadius: BorderRadius.circular(
-                            //         4), // Rectangular border radius
-                            //   ),
-                            // ),
-                            child: const Text(
-                              'PAY',
-                              style:
-                                  TextStyle(fontSize: 20, color: Colors.white),
-                            ),
-                          ),
-                        ],
+    return BlocProvider<PosBloc>(
+      create: (context) => PosBloc(
+          initialState: widget.initialState ?? PosState(),
+          itemRepository: RepositoryProvider.of<ItemRepository>(context)),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('POS System'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Flexible(
+                    flex: 7,
+                    child: Container(
+                      height: containerHeight,
+                      width: double.infinity,
+                      child: const SingleChildScrollView(
+                        child: POSViewTable(),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Flexible(
-                  flex: 4,
-                  child: Container(
-                    color: Colors.blue[50],
-                    child: Center(
-                      child: Text('ss'),
+                  Flexible(
+                    flex: 3,
+                    child: Container(
+                      height: containerHeight,
+                      width: double.infinity,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Column(
+                              children: [
+                                POSViewSearch(),
+                                Padding(
+                                  padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
+                                  child: POSViewCalculation(),
+                                ),
+                              ],
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                // Add your button onPressed logic here
+                              },
+                              // style: ElevatedButton.styleFrom(
+                              //   primary: Colors.red,
+                              //   minimumSize: const Size(double.infinity,
+                              //       50), // Full width and 50 height
+                              //   shape: RoundedRectangleBorder(
+                              //     borderRadius: BorderRadius.circular(
+                              //         4), // Rectangular border radius
+                              //   ),
+                              // ),
+                              child: const Text(
+                                'PAY',
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
-                ),
-                Flexible(
-                  flex: 6,
-                  child: Container(
-                    color: Colors.blue[50],
-                    child: Center(
-                      child: Text('ss'),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Flexible(
+                    flex: 4,
+                    child: Container(
+                      color: Colors.blue[50],
+                      child: const Center(
+                        child: Text('ss'),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  Flexible(
+                    flex: 6,
+                    child: Container(
+                      color: Colors.blue[50],
+                      child: const Center(
+                        child: Text('ss'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
